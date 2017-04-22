53adfefebe8cf613272391a3d9d1900df3b2a647
mlabouardy <mohamed@labouardy.com>
2017-04-22 19:48:39 +0200

    init vagrantfile

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,24 +4,29 @@ sudo bash
 
 ## Install Docker
 
-apt-get update && \
-apt-get install -y apt-transport-https \
-                   ca-certificates \
-                   vim \
-                   curl \
-                   gnupg2 \
-                   software-properties-common
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) \
+         stable"
 
-apt-get update && \
-apt-get install docker-ce -y
+apt-get update 
+
+apt-get install -y docker-ce
+
+## Install vim
+
+apt-get install -y vim
 
 ## Install Docker Compose
 
 curl -L https://github.com/docker/compose/releases/download/1.12.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+
 chmod +x /usr/local/bin/docker-compose
